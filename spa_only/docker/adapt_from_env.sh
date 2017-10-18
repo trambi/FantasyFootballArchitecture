f22e5dea5f54deced97b56523f0ff49d70961195
root <root@dbfe1530852f>
2017-10-18 21:55:04 +0000

    Mod use fork of JManu

--- spa_only/docker/adapt_from_env.sh
+++ spa_only/docker/adapt_from_env.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-sed -i "s#http://192.168.2.2:8080/app.php#${API_ROOT_URL}#" /usr/share/nginx/html/scripts/services.ws.js
+sed -i "s#http://localhost:8080/FantasyFootball2/web/app_dev.php#${API_ROOT_URL}#" /usr/share/nginx/html/scripts/services.ws.js
 nginx -g "daemon off;"
