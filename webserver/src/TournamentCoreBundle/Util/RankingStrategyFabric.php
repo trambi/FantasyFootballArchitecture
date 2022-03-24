d19087a2803de10ece274d24cbc1f5a23e2b5622
Trambi <bertrand.madet@protonmail.com>
2022-03-25 00:08:19 +0100

    :sparkles: Add copy of Rdvbb17 for RdvBB18

--- webserver/src/TournamentCoreBundle/Util/RankingStrategyFabric.php
+++ webserver/src/TournamentCoreBundle/Util/RankingStrategyFabric.php
@@ -28,6 +28,7 @@ use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategy12;
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategy13;
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategy14;
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategy17;
+use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategy18;
 use FantasyFootball\TournamentCoreBundle\Util\Eurobowl\RankingStrategy2017;
 use FantasyFootball\TournamentCoreBundle\Util\LuteceCup\RankingStrategyLutece16;
 
@@ -59,6 +60,8 @@ class RankingStrategyFabric {
             $strategy = new RankingStrategy2017();
         } else if ( 'Lutece16'=== $name ){
             $strategy = new RankingStrategyLutece16();
+        } else if ( 'Rdvbb18' === $name) {
+            $strategy = new RankingStrategy18();
         }
         return $strategy;
     }
@@ -66,6 +69,7 @@ class RankingStrategyFabric {
     static function getNames(){
         return ['Rdvbb1','Rdvbb2To3','Rdvbb4To5','Rdvbb6To8',
                 'Rdvbb9To10','Rdvbb11','Rdvbb12','Rdvbb13',
-                'Rdvbb14','Eurobowl2017','Lutece16', 'Rdvbb17'];
+                'Rdvbb14','Eurobowl2017','Lutece16', 'Rdvbb17',
+                'Rdvbb18'];
     }
 }
