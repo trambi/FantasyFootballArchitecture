aee355afade2636b0d3d6861135d3c13a581e38d
Trambi <bertrand.madet@protonmail.com>
2022-04-14 14:55:14 +0200

    :hammer: Add stop command to kill and remove containers

--- without-compose.bash
+++ without-compose.bash
@@ -1,30 +1,50 @@
 #! /bin/bash
 
+
+readonly clientContainer="ff_webclient"
+readonly serverContainer="ff_webserver"
+readonly dbContainer="database"
+readonly clientImage="localhost/ff_webclient:latest"
+readonly serverImage="localhost/ff_webserver:latest"
+readonly dbImage="localhost/ff_mariadb:latest"
+
 usage(){
   echo "${0} build"
-  echo "  build the three docker images"
+  echo "  build the three container images: ${clientImage},${serverImage} and ${dbImage}"
   echo "${0} start"
-  echo "  start the three docker images"
+  echo "  start the three container images: ${clientImage},${serverImage} and ${dbImage}"
+  echo "  with the container names : ${clientContainer},${serverContainer} and ${dbContainer}"
+  echo "${0} stop"
+  echo "  kill and remove the three containers"
 }
 
 start(){
-  docker run -d -p80:80 -e API_FQDN=${API_FQDN} --name ff_webclient localhost/ff_webclient:latest
-  docker run -d -p8080:80 -e DBHOST=${API_FQDN} -e DBUSER=${DB_USER} -e DBPASSWD=${DB_PASSWORD} --name ff_webserver localhost/ff_webserver:latest
-  docker run -d -p3306:3306 -e MYSQL_DATABASE=tournament -e MYSQL_USER=${DB_USER} -e MYSQL_PASSWORD=${DB_PASSWORD} -e MYSQL_ROOT_PASSWORD=${DB_ROOT_PASSWORD} --name database  -v mydata:/var/lib/mysql -v $(pwd)/mariadb/docker/sql:/docker-entrypoint-initdb.d/:Z localhost/ff_mariadb:latest
+  if [[ -f ".env" ]]
+  then
+    source .env
+  fi
+  docker run -d -p80:80 -e API_FQDN=${API_FQDN} --name ${clientContainer} ${clientImage}
+  docker run -d -p8080:80 -e DBHOST=${API_FQDN} -e DBUSER=${DB_USER} -e DBPASSWD=${DB_PASSWORD} --name ${serverContainer} ${serverImage}
+  docker run -d -p3306:3306 -e MYSQL_DATABASE=tournament -e MYSQL_USER=${DB_USER} -e MYSQL_PASSWORD=${DB_PASSWORD} -e MYSQL_ROOT_PASSWORD=${DB_ROOT_PASSWORD} --name ${dbContainer}  -v mydata:/var/lib/mysql -v $(pwd)/mariadb/docker/sql:/docker-entrypoint-initdb.d/:Z ${dbImage}
+}
+
+stop(){
+  docker kill "${dbContainer}" "${serverContainer}" "${clientContainer}"
+  docker rm "${dbContainer}" "${serverContainer}" "${clientContainer}"
 }
 
 build(){
   (
     cd mariadb/docker
-    docker build -t ff_mariadb:latest .
+    docker build -t ${dbImage} .
   )
   (
     cd webclient/docker
-    docker build -t ff_webclient:latest .
+    docker build -t ${clientImage} .
   )
   (
     cd webserver
-    docker build -t ff_webserver:latest .
+    docker build -t ${serverImage} .
   )
 }
 done=0
@@ -37,6 +57,12 @@ fi
 if [[ "$1" == "start" ]]
 then
   start
+  done=1
+fi
+
+if [[ "$1" == "stop" ]]
+then
+  stop
   done=1
 fi
 
